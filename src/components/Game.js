5067ad33b25b1c92ba54a738b3c0a4db31ef24c4
Jonathan Dahm <jonathan.dahm@sap.com>
2024-04-02 16:01:56 +0200

    MoveContext verschoben

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { getWebSocket } from '../webSocketContext';
-import { MoveContext, getMove } from '../MoveContext';
+import { MoveContext, getMove } from '../context/MoveContext';
 const images = require.context('../images/', false, /\.png$/);
 import { gameData } from './GameData';
 import { usePlayerSettings } from '../context/playerSettingsContext';
